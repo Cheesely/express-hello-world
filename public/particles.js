474bb9b71cdd3a6a6f2ca3dd9816b1775d069572
chelseyfoster <cfoster@udutu.com>
2023-06-21 18:30:55 -0700

    Test on phone

--- public/particles.js
+++ public/particles.js
@@ -11,44 +11,51 @@ document.addEventListener("DOMContentLoaded", () => {
                 if(diceRoll == 1){
                     this.classList.remove('spin');
                     this.classList.add('one');
-                    optionSelected = document.querySelector("#one p").textContent;
+                    optionSelected = document.querySelectorAll("#" + activeBlank.dataset.number + " p span")[0].textContent;
                     activeBlank.innerHTML = optionSelected;
                     activeBlank.classList.remove("blank");
                 } else if(diceRoll == 2){
                     this.classList.remove('spin');
                     this.classList.add('two');
-                    optionSelected = document.querySelector("#one p").textContent;
+                    optionSelected = document.querySelectorAll("#" + activeBlank.dataset.number + " p span")[1].textContent;
                     activeBlank.innerHTML = optionSelected;
                     activeBlank.classList.remove("blank");
                 } else if(diceRoll == 3){
                     this.classList.remove('spin');
                     this.classList.add('three');
-                    optionSelected = document.querySelector("#one p").textContent;
+                    optionSelected = document.querySelectorAll("#" + activeBlank.dataset.number + " p span")[2].textContent;
                     activeBlank.innerHTML = optionSelected;
                     activeBlank.classList.remove("blank");
                 } else if(diceRoll == 4){
                     this.classList.remove('spin');
                     this.classList.add('four');
-                    optionSelected = document.querySelector("#one p").textContent;
+                    optionSelected = document.querySelectorAll("#" + activeBlank.dataset.number + " p span")[3].textContent;
                     activeBlank.innerHTML = optionSelected;
                     activeBlank.classList.remove("blank");
                 } else if(diceRoll == 5){
                     this.classList.remove('spin');
                     this.classList.add('five');
-                    optionSelected = document.querySelector("#one p").textContent;
+                    optionSelected = document.querySelectorAll("#" + activeBlank.dataset.number + " p span")[4].textContent;
                     activeBlank.innerHTML = optionSelected;
                     activeBlank.classList.remove("blank");
                 } else if(diceRoll == 6){
                     this.classList.remove('spin');
                     this.classList.add('six');
-                    optionSelected = document.querySelector("#one p").textContent;
+                    optionSelected = document.querySelectorAll("#" + activeBlank.dataset.number + " p span")[5].textContent;
                     activeBlank.innerHTML = optionSelected;
                     activeBlank.classList.remove("blank");
                 }
+                document.querySelector(".blank").classList.add("active");
             }
             else {
                 this.classList.remove('one','two','three','four','five','six');
                 this.classList.add('spin');
+                const activeOptions = document.querySelector(".options .active");
+                const nextOptions = document.querySelector(".options .active~section");
+                activeOptions.classList.remove("active");
+                nextOptions.classList.remove("hidden");
+                activeOptions.classList.add("hidden");
+                nextOptions.classList.add("active");
             }
         });
     });
